247f3afd0eef3e4c46da6e181744a04736610964
memexpo <dmitry@marscolony.io>
2022-06-09 07:41:58 +0400

    revert derivationPath for hartest

--- migrations/3_gm_instance.js
+++ migrations/3_gm_instance.js
@@ -9,7 +9,7 @@ const GM = artifacts.require("GameManager");
 const AM = artifacts.require("AvatarManager");
 const MC = artifacts.require("MC");
 const CLNY = artifacts.require("CLNY");
-// const ProxyAdmin = artifacts.require("ProxyAdmin");
+const ProxyAdmin = artifacts.require("ProxyAdmin");
 const Lootboxes = artifacts.require("Lootboxes");
 
 module.exports = async (deployer, network, addresses) => {
@@ -17,11 +17,11 @@ module.exports = async (deployer, network, addresses) => {
     return; // this file for manual migrations; pass in tests
   }
   const inst = await deployer.deploy(GM);
-  // const lb = await deployer.deploy(Lootboxes, 'https://lootboxes-harmony.marscolony.io/');
-  // const PA = await ProxyAdmin.at('0xc470b22A8D173a0DA50191A4A0E5e2b42f6B6009');
-  // await PA.upgrade('0xc65F8BA708814653EDdCe0e9f75827fe309E29aD', GM.address);
-  // const gm = await GM.at('0xc65F8BA708814653EDdCe0e9f75827fe309E29aD');
-  // await gm.setLootboxesAddress(Lootboxes.address);
+  const lb = await deployer.deploy(Lootboxes, 'https://lootboxes-harmony.marscolony.io/');
+  const PA = await ProxyAdmin.at('0xc470b22A8D173a0DA50191A4A0E5e2b42f6B6009');
+  await PA.upgrade('0xc65F8BA708814653EDdCe0e9f75827fe309E29aD', GM.address);
+  const gm = await GM.at('0xc65F8BA708814653EDdCe0e9f75827fe309E29aD');
+  await gm.setLootboxesAddress(Lootboxes.address);
 
 
 };
