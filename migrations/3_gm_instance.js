51d12de11954cee8601d6e63335dc3f1c95f83d4
memexpo <dmitry@marscolony.io>
2022-06-13 00:14:13 +0400

    ProxyAdmin artifact unignore

--- migrations/3_gm_instance.js
+++ migrations/3_gm_instance.js
@@ -25,59 +25,12 @@ module.exports = async (deployer, network, addresses) => {
 
 
 
-  // // const mcl = await MartianColonists.at('0x76F8089064f58586471f38824da290913E6a5454');
   await deployer.deploy(GameManager);
-  await GameManager.deployed();
-  // // const mm = await MM.at('0xf91719366dec915741E57b246f97048D4b5D338e');
-
-  // // await deployer.deploy(SalesManager);
-  // // const sm = await SalesManager.deployed();
 
   const PA = await ProxyAdmin.at('0xa85Dda80Dd10ecE178e59B964Bc094AdE4fa4f31');
+  console.log(await PA.getProxyImplementation('0xCAFAeD55fEfEd74Ca866fE72D65CfF073eb42797'));
   await PA.upgrade('0xCAFAeD55fEfEd74Ca866fE72D65CfF073eb42797', GameManager.address)
 
-  // const gm = await GameManager.at('0xCAFAeD55fEfEd74Ca866fE72D65CfF073eb42797');
-  // const mc = await MC.at('0xBF5C3027992690d752be3e764a4B61Fc6910A5c0');
-  // await gm.setClnyPerSecond(ether('100000').div(new BN((24 * 60 * 60).toString())))
-  // await gm.updatePool(await gm.CLNYAddress())
-  // await gm.setTotalShareFromTotalSupply();
-  // console.log(+await gm.totalShare())
-  // await gm.startShareSystem(1653403273);
-  // const ids = await mc.allTokensPaginate(60, 80);
-  // console.log(ids.map(o=>+o));
-  // const tx = await gm.setBackendSigner('0xfbfe71afec1b7eb860542111b7a2ce5afaa2c11f');
-  // console.log({ tx });
 
-  // MM
-  // await deployProxy(MM, [
-  //   addresses[0],
-  //   mcl.address,
-  //   '0x85f8e0aBdb0f45D8488ca608Ac6327Edd3705de2',
-  //   '0xBF5C3027992690d752be3e764a4B61Fc6910A5c0'
-  // ], { deployer });
-  // await gm.setMissionManager('0xf91719366dec915741E57b246f97048D4b5D338e');
-  // await mm.setGameManager('0xCAFAeD55fEfEd74Ca866fE72D65CfF073eb42797');
-
-  // END MM
-
-  // AM
-  // await deployProxy(AM, [addresses[0], mcl.address], { deployer });
-
-  // await mcl.setAvatarManager(AM.address);
-  // await gm.setAvatarAddress(AM.address);
-
-  // END AM
-
-  // await deployer.deploy(GameManager);
-  // const PA = await ProxyAdmin.at('0xa85Dda80Dd10ecE178e59B964Bc094AdE4fa4f31');
-  // await PA.upgrade('0xCAFAeD55fEfEd74Ca866fE72D65CfF073eb42797', GameManager.address)
-
-  // const sm = await SalesManager.at('0x361812f92f5B072E4972a3fB47b0e515dEbB5610');
-
-  // await sm.setMC('0xb5D95034171733F3D636B49e5f4703d7d906b1a4');
-
-  // const mc = await MC.at('0xb5D95034171733F3D636B49e5f4703d7d906b1a4');
-
-  // await mc.setSalesManager('0x361812f92f5B072E4972a3fB47b0e515dEbB5610');
   
 };
