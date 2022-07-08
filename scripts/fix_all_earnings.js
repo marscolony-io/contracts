e9d6cd7fccfb287af15d47d156c25039816aa01e
memexpo <dmitry@marscolony.io>
2022-07-08 19:39:15 +0400

    fix fix_all_earnings script

--- scripts/fix_all_earnings.js
+++ scripts/fix_all_earnings.js
@@ -1,17 +1,32 @@
 const GM = artifacts.require('GameManager');
+const MC = artifacts.require('MC');
 
 module.exports = async (callback) => {
   try {
       
     const [DAO] = await web3.eth.getAccounts();
     let _GM;
+    let _MC;
     
-    _GM = await GM.at('0x0D112a449D23961d03E906572D8ce861C441D6c3'); // harmony
+    _GM = await GM.at('0x0Dd5dDaC089613F736e89F81E16361b09c7d53C6'); // harmony
+    _MC = await MC.at(await _GM.MCAddress());
+
+    const allTokenIds = [];
+
+    for (let i = 0; i < 210; i++) {
+      const tokens = (await _MC.allTokensPaginate(i * 100, (i + 1) * 100 - 1)).map(z => +z);
+      allTokenIds.push(...tokens);
+      if (tokens.length === 0) {
+        break;
+      }
+    }
+
+    console.log(allTokenIds);
   
-    let tokenIds = []
-    for (let i = 1; i <= 21000; i++) {
-      tokenIds.push(i)
-      if (i % 1000 == 0) {
+    let tokenIds = [];
+    for (let i = 0; i < allTokenIds.length; i++) {
+      tokenIds.push(allTokenIds[i])
+      if ((i + 1) % 1000 == 0 || i === allTokenIds.length - 1) {
         console.log(i)
         await _GM.fixEarnings(tokenIds, { from: DAO });
         tokenIds = []
